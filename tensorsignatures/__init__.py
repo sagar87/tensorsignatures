120cc639db1acc0122c2862039700819756caf63
Harald Sagar Vöhringer <Sagar@b1161-c542.local>
2019-08-22 10:32:40 +0100

    fixed __init__.py

--- tensorsignatures/__init__.py
+++ tensorsignatures/__init__.py
@@ -5,3 +5,9 @@
 __author__ = """Harald Sager Vohringer"""
 __email__ = 'harald.voeh@gmail.com'
 __version__ = '0.1.0'
+
+
+from tensorsignatures.tensorsignatures import *
+from tensorsignatures.config import *
+from tensorsignatures.model import *
+from tensorsignatures.util import *
