da87783437190165955eed04b7ef06ce10e0f359
Harald Sagar Vöhringer <Sagar@b1161-c542.local>
2019-08-22 11:07:13 +0100

    fixed old imports

--- tensorsignatures/__init__.py
+++ tensorsignatures/__init__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
+from tensorsignatures.tensorsignatures import *
+from tensorsignatures.config import *
+from tensorsignatures.util import *
 
 """Top-level package for tensorsignatures."""
 
 __author__ = """Harald Sager Vohringer"""
 __email__ = 'harald.voeh@gmail.com'
 __version__ = '0.1.0'
-
-
-from tensorsignatures.tensorsignatures import *
-from tensorsignatures.config import *
-from tensorsignatures.util import *
