4c49fcc4df0cc8a195a5988fa4f05c9075eaec07
Harald Sagar Vöhringer <Sagar@b1161-c542.local>
2019-08-22 17:03:25 +0100

    first docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'tensorflow']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'tensorflow', 'h5py']
 for mod_name in MOCK_MODULES:
 	sys.modules[mod_name] = mock.Mock()
 
