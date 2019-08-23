786081e2e652144fdf3fd2e4b9e15f8693ca4e0a
Harald Sagar Vöhringer <Sagar@b1161-c542.local>
2019-08-23 18:48:35 +0100

    updates to the documentation

--- tensorsignatures/tensorsignatures.py
+++ tensorsignatures/tensorsignatures.py
@@ -51,26 +51,27 @@ def define_scope(function, scope=None, *args, **kwargs):
     return decorator
 
 class TensorSignature(object):
-	"""Heart of TensorSignatures.
+    """Heart of TensorSignatures.
 
-	Args:
-		snv: (3 x 3 x d_1, ..., d_m x p x n) input SNV tensor with shape. 
-			TensorSignature expects the first two dimension to represent
-			transcription (3) and replication (3), while the last two 
-			dimensions should contain the mutation types (p) and
-			samples (n). Dimensions d_1 ... d_m may represent arbtrary
-			genomic states.
-		other: (q x n) mutation count matrix with q mutation types and
-			n samples
-		N: (3 x 3 x d_1, ..., d_m x p x 1) optional normalization tensor
-			containing trinucleotide frequencies for each genomic state
-		objective: likelihood function with which mutation counts are
-			modeled. Currently, negative binomial or poisson.
+    Args:
+        snv: (3 x 3 x d_1, ..., d_m x p x n) input SNV tensor with shape. 
+            TensorSignature expects the first two dimension to represent
+            transcription (3) and replication (3), while the last two 
+            dimensions should contain the mutation types (p) and
+            samples (n). Dimensions d_1 ... d_m may represent arbtrary
+            genomic states.
+        other: (q x n) mutation count matrix with q mutation types and
+            n samples
+        N: (3 x 3 x d_1, ..., d_m x p x 1) optional normalization tensor
+            containing trinucleotide frequencies for each genomic state
+        objective: likelihood function with which mutation counts are
+            modeled. Currently, negative binomial or poisson.
 
 
-	"""
+    """
     
-    def __init__(self, snv, other, N=None, objective='nbconst', dtype=tf.float32, verbose=True, seed=None, **kwargs):
+    def __init__(self, snv, other, N=None, objective='nbconst', 
+        dtype=tf.float32, verbose=True, seed=None, **kwargs):
         assert(len(snv.shape) >= 5)
         self.verbose = verbose
         self.seed = seed
