7d398ec896de9704475efd94777325ad6464e480
Harald Sagar Vöhringer <Sagar@b1161-c542.local>
2019-09-19 12:54:16 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ from subprocess import check_output, CalledProcessError
 
 try:
     num_gpus = len(check_output(['nvidia-smi', '--query-gpu=gpu_name', '--format=csv']).decode().strip().split('\n'))
-    tf = 'tensorflow-gpu>nvidia=1.12' if num_gpus > 1 else 'tensorflow'
+    tf = 'tensorflow-gpu>=1.10' if num_gpus > 1 else 'tensorflow'
 except CalledProcessError:
-    tf = 'tensorflow>=1.12'
+    tf = 'tensorflow>=1.10'
 
 
 with open('README.rst') as readme_file:
